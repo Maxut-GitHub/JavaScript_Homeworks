0ba1641c53cc2e48fa0b394b6124a46d9e5e04a0
Maxim Babukhin <maxut.105.game@gmail.com>
2024-01-06 20:20:52 +0300

    2

--- Homework7 VOWELS_ARR/index.js
+++ Homework7 VOWELS_ARR/index.js
@@ -14,12 +14,33 @@ function vowers(anyString) {
 	return count;
 }
 
-function vowersForEach(anyString) {}
-function vowersFilter(anyString) {}
+function vowersForEach(anyString) {
+	let count = 0;
+	const vowels = "ауоиэыяюеё";
+	let arrString = anyString.toLowerCase().split("");
+	arrString.forEach((element) => {
+		if (vowels.indexOf(element) > -1) {
+			count++;
+		}
+	});
+	return count;
+}
+
+function vowersFilter(anyString) {
+	const vowels = "ауоиэыяюеё";
+	let arrString = anyString.toLowerCase().split("");
+	let filterString = arrString.filter(function (element) {
+		if (vowels.indexOf(element) > -1) {
+			return true;
+		} else return false;
+	});
+	return filterString.length;
+}
+
 function vowersReduse(anyString) {}
 
 alert(`
-vowersForEach - ${vowersForEach(string)} 
-vowersFilter - ${vowersFilter(string)} 
+vowersForEach - ${vowersForEach(string)}
+vowersFilter - ${vowersFilter(string)}
 vowersReduse - ${vowersReduse(string)}
 `);
