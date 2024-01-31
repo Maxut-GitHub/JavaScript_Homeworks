4c43ceb8a4974a326ebb62d32570f6ae3298e15b
Maxim Babukhin <maxut.105.game@gmail.com>
2024-01-31 17:49:36 +0300

    3

--- Homework10 VALID_FORM/index.js
+++ Homework10 VALID_FORM/index.js
@@ -9,14 +9,40 @@ function createError(text) {
 	return errorText;
 }
 
-let el1 = document.getElementById(`developers`);
-el1.addEventListener(`blur`, validDifficult);
+//Дал слушателя всем нужным элементам
+{
+	let el1 = document.getElementById(`developers`);
+	el1.addEventListener(`blur`, validDifficult);
 
-let el2 = document.getElementById(`siteName`);
-el2.addEventListener(`blur`, validSimple);
+	let el2 = document.getElementById(`siteName`);
+	el2.addEventListener(`blur`, validSimple);
 
-let el3 = document.getElementById(`siteURL`);
-el3.addEventListener(`blur`, validSimple);
+	let el3 = document.getElementById(`siteURL`);
+	el3.addEventListener(`blur`, validSimple);
+
+	let el4 = document.getElementById(`reliaseDate`);
+	el4.addEventListener(`blur`, validSimple);
+
+	let el5 = document.getElementById(`visitors`);
+	el5.addEventListener(`blur`, validSimple);
+
+	let el6 = document.getElementById(`email`);
+	el6.addEventListener(`blur`, validSimple);
+
+	let el7 = document.getElementById(`catalog`);
+	el7.addEventListener(`blur`, validSelect);
+
+	let el8 = document.getElementsByClassName(`radio`);
+	for (let el of el8) {
+		el.addEventListener(`blur`, validRadio);
+	}
+
+	let el9 = document.getElementById(`votes`);
+	el9.addEventListener(`blur`, validСheckbox);
+
+	let el10 = document.getElementById(`description`);
+	el10.addEventListener(`blur`, validSimple);
+}
 
 //Проверка на пустоту, цифры и длину более 3 символов
 function validDifficult(el) {
@@ -59,3 +85,60 @@ function validSimple(el) {
 		return;
 	}
 }
+
+//Проверка списка
+function validSelect(el) {
+	let elementValue = el.target.value;
+	console.log(elementValue);
+	let parentEl = el.target.parentNode;
+	//Удаление красной ошибки, если она найдена
+	let errorEl = parentEl.getElementsByTagName(`p`);
+	if (errorEl[0]) {
+		parentEl.removeChild(errorEl[0]);
+	}
+	//Далее все проверки
+	if (elementValue === "3") {
+		parentEl.appendChild(createError("← к сожалению, недоступно"));
+		return;
+	}
+}
+
+//Проверка радио
+function validRadio(el) {
+	let elementValue = el.target.value;
+	console.log(elementValue);
+	let parentEl = el.target.parentNode;
+	//Удаление красной ошибки, если она найдена
+	let errorEl = parentEl.getElementsByTagName(`p`);
+	if (errorEl[0]) {
+		parentEl.removeChild(errorEl[0]);
+	}
+	//Далее все проверки
+	if (elementValue === "1") {
+		parentEl.appendChild(createError("← бесплатно нельзя!"));
+		return;
+	}
+}
+
+//Проверка чекбокса
+function validСheckbox(el) {
+	let parentEl = el.target.parentNode;
+	//Удаление красной ошибки, если она найдена
+	let errorEl = parentEl.getElementsByTagName(`p`);
+	if (errorEl[0]) {
+		parentEl.removeChild(errorEl[0]);
+	}
+	//Далее все проверки
+	if (!el.target.checked) {
+		parentEl.appendChild(createError("← Отзывы должны быть разрешены!"));
+		return;
+	}
+}
+
+//отмена отправки формы
+let form = document.getElementsByTagName(`form`);
+form[0].addEventListener(`submit`, function (event) {
+	event.preventDefault();
+});
+
+function submitButton() {}
